8b9b94016d2d6299a635ee45f809bbda4be4b1dd
Damian Sima <damiansima@gmail.com>
2015-01-15 18:34:44 -0300

    adding tests for ws consumer

---
+++ munit-integration-tests/src/test/java/org/mule/munit/WsConsumerMunitTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.munit;
+
+
+import org.mule.munit.runner.java.AbstractMuleSuite;
+
+public class WsConsumerMunitTest extends AbstractMuleSuite
+{
+
+    @Override
+    public String getConfigResources()
+    {
+        return "ws-consumer-test.xml";
+    }
+}
